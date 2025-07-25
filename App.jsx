dcf3ffdc7345694f00a55b4b467646d861e2a40b
mancher01 <rmanchester@oncboces.org>
2025-07-25 11:20:22 -0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,6 @@
+import PDPlannerApp from './PdplannerApp';
+import guidingQuestions from './GuidingQuestionsConfig';
+
+export default function App() {
+  return <PDPlannerApp guidingQuestions={guidingQuestions} />;
+}
